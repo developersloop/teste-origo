31081d9961219ce918ef54134bc55352b4d68c42
vitor <loopinfinite.all@gmail.com>
2023-02-16 11:33:18 -0300

    finaliza teste

--- cypress/integration/API/links.specs.js
+++ cypress/integration/API/links.specs.js
@@ -2,7 +2,9 @@
 
 let mock = require('../../fixtures/example.json')
 
-const habilitEditOrDelete = (id = 36) => {
+// id mock sempre acrescido de 1 (1,2,3) isso permitira acrescentar dados na base e testar mais de mais uma vez
+const idMock = 47;
+const habilitEditOrDelete = (id = idMock) => {
     return cy.request('GET', `${Cypress.env('url')}/disabled-for-test/${id}`)
         .then(res => {
             expect(res.status).to.equal(200)
@@ -13,7 +15,7 @@ const habilitEditOrDelete = (id = 36) => {
 }
 
 describe('Testing API links', function() {
-    it.skip('Success status 200 list links', function() {
+    it('Success status 200 list links', function() {
         cy.request({
             method: 'GET',
             url: Cypress.env('url'),
@@ -28,11 +30,11 @@ describe('Testing API links', function() {
         })
     })
 
-    it.only('Success status 201 created new shorts links', function() {
+    it('Success status 201 created new shorts links', function() {
         cy.request({
             method: 'post',
             url: Cypress.env('url'),
-            body: { link: mock.links[0]},
+            body: mock.links[0],
             failOnStatusCode: false,
         })
         .then(res => {
@@ -48,18 +50,17 @@ describe('Testing API links', function() {
             else  {
                 expect(res.status).to.equal(200)
                 expect(res.isOkStatusCode).to.be.true
-                expect(res.body).to.have.property('message')
-                expect(mock.messages.includes(res.body.message)).to.be.true
+                expect(res.body).to.not.have.property('message')
             }
         })
     })
 
-    it.skip('Success status 200 update link', function() {
+    it('Success status 200 update link', function() {
         habilitEditOrDelete()
             .then(res => {
                 cy.request({
                     method: 'PUT',
-                    url: `${Cypress.env('url')}/36`,
+                    url: `${Cypress.env('url')}/${idMock}`,
                     body: mock.links[1],
                     failOnStatusCode: false,
                 })
@@ -83,18 +84,19 @@ describe('Testing API links', function() {
                 })
             })
     })
+   // para ver o teste rodando por inteiro retirar o .skip
+   // para ver o teste persistando o link na base acrescentar o .skip
     it.skip('Success status 202 deleted link', function() {
         habilitEditOrDelete()
         .then(res => {
             cy.request({
                 method: 'delete',
-                url: `${Cypress.env('url')}/36`,
+                url: `${Cypress.env('url')}/${idMock}`,
                 failOnStatusCode: false,
             })
             .then(res => {
-                console.log(res)
-                if (res.status == 404) {
-                    expect(res.status).to.equal(404)
+                if (res.status == 414) {
+                    expect(res.status).to.equal(414)
                     expect(res.isOkStatusCode).to.be.false
                     expect(res.body).to.have.property('message')
                     expect(!res.body.data.length).to.be.true
